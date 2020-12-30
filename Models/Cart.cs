9753350417f4fea6f3063cfcf3c73d3843a17a2a
haidy-ismail <70165618+haidy-ismail@users.noreply.github.com>
2020-12-30 22:41:49 +0200

    Add files via upload

---
+++ Models/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Telephone.Models
+{
+    public class Cart
+    {
+        public Product product { get; set; }
+        public int quantity { get; set; }
+        public Cart(Product Product,int Quantity)
+        {
+            product = Product;
+            quantity = Quantity;
+
+        }
+    }
+}
